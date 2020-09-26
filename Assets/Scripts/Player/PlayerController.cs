69eac8a1ac85c88205d450bf065b00042cb8dd4c
Justin Lu <Justin_Lu@student.uml.edu>
2020-09-26 11:32:49 -0400

    Organize the folder

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,23 +5,24 @@ using UnityEngine.Serialization;
 public class PlayerController : MonoBehaviour
 {
     //Related to Speed of Player Movement and Jumping
-    [SerializeField] private float Speed= 40f;
-    [SerializeField] private float JumpForce= 10f;
-    [Range(0,0.3f)] [SerializeField] private float smoothTime = 0.3f;
-    
+    public float maxSpeed = 12;
+    public float smoothTime = 0.3f;
+    public bool debug = true;
+    public float initialJumpVelo = 10f;
 
-    //Related to Checking Ground when Player Jump
-    public Transform groundCheck;
+
     public LayerMask groundMask;
-
+    public Transform groundCheck;
     private Rigidbody2D rb2d;
 
     private Vector3 currentVel = Vector3.zero;
-    //private bool facingRight = true;
+    private bool facingRight = true;
 
-    private bool onGround = true;
-    private const float groundCheckRadius = .2f;
+    private bool onGround = false;
 
+    private GameObject currentGround;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -31,61 +32,70 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal_input = Input.GetAxisRaw("Horizontal") * Speed;
-        bool isJump = Input.GetKey(KeyCode.Space);
 
-        Movement(horizontal_input*Time.fixedDeltaTime, isJump);
     }
     private void FixedUpdate()
     {
-        if (!onGround) CheckGround();
+       move();
+       if (!onGround) checkGround();
     }
 
     //basic movement
-    void Movement(float h_input,bool isJump)
+    void move()
     {
+        bool isJump = Input.GetKey(KeyCode.Space);
+        float h = Input.GetAxisRaw("Horizontal");
 
-        if (h_input != 0f)
+        if (h != 0f)
         {
-            Vector3 newVel = new Vector3(h_input*10f, rb2d.velocity.y);
+            Vector3 newVel = new Vector3(h * maxSpeed, rb2d.velocity.y);
             rb2d.velocity = Vector3.SmoothDamp(rb2d.velocity,newVel,ref currentVel,smoothTime);
         }
 
-        //if ((h_input > 0 && !facingRight) || (h_input < 0 && facingRight))
-        //{
-        //    Flip();
-       //}
+        if ((h > 0 && !facingRight) || (h < 0 && facingRight))
+        {
+            Flip();
+        }
 
+        Debug.Log(onGround);
         if (onGround && isJump)
         {
             onGround = false;
-            rb2d.AddForce(new Vector2(0f, JumpForce*10f));
+            rb2d.AddForce(new Vector2(0f, initialJumpVelo), ForceMode2D.Impulse);
         }
     }
 
+
+    
     //when player jumped, check the bottom of player to check if the the radius we set hit with ground layer mask
-   private void CheckGround()
+   private void checkGround()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundCheckRadius, groundMask);
-        for (int i = 0; i < colliders.Length; i++)
+        Vector3 rightCornerOffset = groundCheck.position + Vector3.right * transform.localScale.x/2;
+        Vector3 leftCornerOffset = groundCheck.position - Vector3.right * transform.localScale.x/2;
+
+        if(debug)
         {
-            if (colliders[i].gameObject != gameObject)
-            {
-                onGround = true;
-                break;
-            }
+            Debug.DrawRay(rightCornerOffset, -Vector3.up * .04f, Color.red, 3f);
+            Debug.DrawRay(leftCornerOffset, -Vector3.up * .04f, Color.red, 3f);
         }
+
+
+        if(rb2d.velocity.y < 0 && (Physics2D.Raycast(rightCornerOffset, -Vector3.up, 0.04f, groundMask) ||
+        Physics2D.Raycast(leftCornerOffset, -Vector3.up, 0.04f, groundMask)))
+        {
+            onGround = true;
+        }        
     }
 
     //flip the player based on the direction of player moving (can't really see that rn because it's just a rectangle with flat color)
-    //private void Flip()
-    //{
+    private void Flip()
+    {
         // Switch the way the player is labelled as facing.
-     //   facingRight = !facingRight;
+        facingRight = !facingRight;
 
         // Multiply the player's x local scale by -1.
-     //   Vector3 theScale = transform.localScale;
-     //   theScale.x *= -1;
-     //   transform.localScale = theScale;
-   //}
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
