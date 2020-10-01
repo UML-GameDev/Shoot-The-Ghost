854b49e63cb084775cbe9f216b653be21a991a0c
Tylerlordan <71417203+Tylerlordan@users.noreply.github.com>
2020-09-30 21:59:48 -0400

    Added basic player health mechanic
    no death state atm

---
+++ Assets/Scripts/DataClasses/BulletData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "BulletData", menuName = "ScriptableObjects/BulletData", order = 1)]
+public class BulletData : ScriptableObject
+{
+    public float bulletSpeed = 1f;
+    public float lifeDuration = 2f;
+    public float bulletDamage = 10f;
+}
