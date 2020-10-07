cf8b89ab4288b82a000809101eecd00fe03e9438
MatriXcel <wasiimouro@gmail.com>
2020-10-07 01:26:28 -0400

    addressed problem of health bar flipping with player

--- Assets/Scripts/Generic/HealthBarUpdater.cs
+++ Assets/Scripts/Generic/HealthBarUpdater.cs
@@ -5,18 +5,24 @@ public class HealthBarUpdater : MonoBehaviour
     float initHealthScaleX;
     
     public GameObject owner;
+    public Transform healthBarTrans;
 
     void Start()
     {
         owner.GetComponent<HealthUpdatable>().OnHealthUpdated.AddListener(updateHealth);
-        initHealthScaleX = transform.localScale.x;
+        initHealthScaleX = healthBarTrans.localScale.x;
     }
     void updateHealth(float currHealth)
     {
         float newScaleX = (currHealth / 100f) * initHealthScaleX;
-        float scaleDelta = newScaleX - transform.localScale.x;
+        float scaleDelta = newScaleX - healthBarTrans.localScale.x;
+        
+        healthBarTrans.localScale = new Vector3(newScaleX, healthBarTrans.localScale.y, 0);
+        healthBarTrans.position += new Vector3(scaleDelta/2, 0, 0);
+    }
 
-        transform.localScale = new Vector3(newScaleX, transform.localScale.y, 0);
-        transform.position += new Vector3(scaleDelta/2, 0, 0);
+    void Update()
+    {
+        transform.LookAt(transform.position + new Vector3(0, 0, 4));
     }
 }
